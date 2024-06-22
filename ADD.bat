3a6382bb312b3d8064837ce2487772aee067f62a
idkwhereisthisname <brobxuusa1@gmail.com>
2024-06-22 16:59:16 +0200

    Add files via upload

--- ADD.bat
+++ ADD.bat
@@ -18,29 +18,34 @@ ADD.REG
 cd %SYSTEMROOT%\System32
 @echo Taking ownership of Display.dll...
 takeown /f %SYSTEMROOT%\System32\Display.dll
+icacls Display.dll "%SYSTEMROOT%\System32\Display.dll" /grant Users:F
 ren %SYSTEMROOT%\System32\Display.dll Displayold.dll
 cd %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\DISPLAYCPL\DLL
 xcopy %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\DISPLAYCPL\DLL\Display.dll %SYSTEMROOT%\System32\
 cd %SYSTEMROOT%\System32\your-locale\
 @echo Taking ownership of Display.dll.mui...
 takeown /f %SYSTEMROOT%\System32\your-locale\Display.dll.mui
+icacls Display.dll "%SYSTEMROOT%\System32\your-locale" /grant Users:F
 ren %SYSTEMROOT%\System32\your-locale\Display.dll.mui Displayold.dll.mui
 cd %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\displaycpl\DLL
-xcopy %USERPROFILE%\Downloads\DISPLAYCPL\DLL\Display.dll.mui %SYSTEMROOT%\System32\your-locale
+xcopy %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\DISPLAYCPL\DLL\Display.dll.mui %SYSTEMROOT%\System32\your-locale
 cd %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\DISPLAYCPL\REG
 ADD.REG
 cd %SYSTEMROOT%\SystemResources
-@echo Taking ownership of shell32.dll.mun...
-takeown /f %SYSTEMROOT%\SystemResources\shell32.dll.mun
+@echo Taking ownership of Shell32.dll.mun...
+takeown /f %SYSTEMROOT%\SystemResources\Shell32.dll.mun
+icacls Display.dll "%SYSTEMROOT%\SystemResources\Shel32.dll.mun" /grant Users:F
 ren %SYSTEMROOT%\SystemResources\Shell32.dll.mun Shell32old.dll.mun
+@echo Taking ownership of Shell32.dll.mui
 cd %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\DISPLAYCPL\SHELL32\
 xcopy %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\DISPLAYCPL\SHELL32\Shell32.dll.mun %SYSTEMROOT%\SystemResources
 cd ..
 cd %SYSTEMROOT%\System32\your-locale
 :: i wanted to put the entire directory to avoid confusion lol
 takeown /f %SYSTEMROOT%\System32\your-locale\Shell32.dll.mui
+icacls Display.dll "%SYSTEMROOT%\System32\your-locale\Shell32.dll.mui" /grant Users:F
 ren %SYSTEMROOT%\System32\your-locale\Shell32.dll.mui Shell32old.dll.mui
-cd %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\SHELL32
+cd %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\DISPLAYCPL\SHELL32
 xcopy %USERPROFILE%\Downloads\GBTCPL11\GBTCPL11-main\DISPLAYCPL\SHELL32\Shell32.dll.mui %SYSTEMROOT%\System32\your-locale\
 reg add HKEY_CLASSES_ROOT\CLSID\{C555438B-3C23-4769-A71F-B6D3D9B6053A}\Instance\InitPropertyBag /v ResourceDLL /t REG_EXPAND_SZ /d %SYSTEMROOT%\System32\Display.dll
 reg add HKEY_CLASSES_ROOT\CLSID\{C555438B-3C23-4769-A71F-B6D3D9B6053A}\Instance\InitPropertyBag /v ResourceID /t REG_DWORD /d 65
