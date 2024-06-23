e91bc865e6432221f9386e907857041e9e3daff5
idkwhereisthisname <brobxuusa1@gmail.com>
2024-06-23 22:42:58 +0200

    Update ADD.bat

--- ADD.bat
+++ ADD.bat
@@ -78,6 +78,9 @@ xcopy %USERPROFILE%\Downloads\GBTCPL11-main\WINUPD\en-US\wucltux.dll.mui %SYSTEM
 cd %USERPROFILE%\downloads\gbtcpl11-main\WINUPD\reg
 WUCLTUX.REG
 regsvr32 wucltux.dll
+@echo.
+@echo Credits to a WinClassic thread (https://winclassic.net/thread/1779/restoring-control-panel-pages-links)
+@echo.
 @echo Done! You can now close this window
 @explorer
 exit
