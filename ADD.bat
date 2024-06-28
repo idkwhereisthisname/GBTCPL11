c359b380c7942cbcaf43d089927aafd12183463c
idkwhereisthisname <brobxuusa1@gmail.com>
2024-06-28 19:47:25 +0200

    Add files via upload

--- ADD.bat
+++ ADD.bat
@@ -31,10 +31,14 @@ ADD.REG
 @echo.
 @echo Restoring the classic Windows Update applet... (this is just for decoration, and doesnt acutally update anything.)
 @echo.
-cd %USERPROFILE%\Downloads\GBTCPL11-main\WINUPD\
-xcopy %USERPROFILE%\Downloads\GBTCPL11-main\WINUPD\wucltux.dll %SYSTEMROOT%\System32
-xcopy %USERPROFILE%\Downloads\GBTCPL11-main\WINUPD\en-US\wucltux.dll.mui %SYSTEMROOT%\System32\en-US
-cd %USERPROFILE%\downloads\gbtcpl11-main\WINUPD\reg
+cd ..
+cd WINUPD\
+xcopy wucltux.dll %SYSTEMROOT%\System32
+cd en-US
+xcopy wucltux.dll.mui %SYSTEMROOT%\System32\en-US
+cd WINUPD\reg
+cd ..
+cd reg
 WUCLTUX.REG
 regsvr32 wucltux.dll
 @echo.
@@ -48,18 +52,18 @@ regsvr32 wucltux.dll
 @echo.
 cd %SYSTEMROOT%\System32
 @echo Taking ownership of Display.dll...
-takeown /f %SYSTEMROOT%\System32\Display.dll
-icacls "%SYSTEMROOT%\System32\Display.dll" /grant USERNAME:F
+takeown /f Display.dll
+icacls Display.dll /grant USERNAME:F
 ren %SYSTEMROOT%\System32\Display.dll Displayold.dll
 cd %USERPROFILE%\Downloads\GBTCPL11-main\DISPLAYCPL\DLL
-xcopy %USERPROFILE%\Downloads\GBTCPL11-main\DISPLAYCPL\DLL\Display.dll %SYSTEMROOT%\System32\
+xcopy Display.dll %SYSTEMROOT%\System32\
 cd %SYSTEMROOT%\System32\en-US\
 @echo Taking ownership of Display.dll.mui...
-takeown /f %SYSTEMROOT%\System32\en-US\Display.dll.mui
-icacls "%SYSTEMROOT%\System32\en-US\Display.dll.mui" /grant USERNAME:F
-ren %SYSTEMROOT%\System32\en-US\Display.dll.mui Displayold.dll.mui
+takeown /f Display.dll.mui
+icacls "Display.dll.mui" /grant USERNAME:F
+ren "Display.dll.mui" "Displayold.dll.mui"
 cd %USERPROFILE%\Downloads\GBTCPL11-main\displaycpl\DLL
-xcopy %USERPROFILE%\Downloads\GBTCPL11-main\DISPLAYCPL\DLL\Display.dll.mui %SYSTEMROOT%\System32\en-US
+xcopy Display.dll.mui %SYSTEMROOT%\System32\en-US
 cd %USERPROFILE%\Downloads\GBTCPL11-main\DISPLAYCPL\REG
 ADD.REG
 cd %SYSTEMROOT%\SystemResources
@@ -69,21 +73,19 @@ taskkill /f /im explorer.exe
 : icacls "%SYSTEMROOT%\SystemResources\Shell32.dll.mun" /grant USER:F
 : ren "%SYSTEMROOT%\SystemResources\Shell32.dll.mun" "Shell32old.dll.mun"
 : cd %USERPROFILE%\Downloads\GBTCPL11-main\DISPLAYCPL\SHELL32
-takeown /f %SYSTEMROOT%\SystemResources
-icacls "%SYSTEMROOT%\SystemResources" /grant USERNAME:F
-xcopy "%USERPROFILE%\Downloads\GBTCPL11-main\DISPLAYCPL\SHELL32\shell32.dll.mun" %SYSTEMROOT%\SystemResources
-: also old method which doesnt work
-: cd %SYSTEMROOT%\System32\en-su
-: takeown /f %SYSTEMROOT%\System32\en-su\shell32.dll.mui
-: icacls "%SYSTEMROOT%\System32\en-su\shell32.dll.mui" /grant USER:F
-takeown /f %SYSTEMROOT%\System32\en-US\
-icacls "%SYSTEMROOT%\System32\en-US" /grant USERNAME:F
-ren %SYSTEMROOT%\System32\en-US\shell32.dll.mui shell32old.dll.mui
+cd %SYSTEMROOT%\SystemResources
+takeown /f Shell32.dll.mun
+icacls "Shell32.dll.mun" /grant USERNAME:F
+ren "Shell32.dll.mun" "Shell32old.dll.mun"
+cd %SYSTEMROOT%\System32\en-US
+takeown /f Shell32.dll.mui
+icacls "Shell32.dll.mui" /grant USERNAME:F
+ren Shell32.dll.mui Shell32old.dll.mui
 cd %USERPROFILE%\Downloads\GBTCPL11-main\DISPLAYCPL\SHELL32
-xcopy %USERPROFILE%\Downloads\GBTCPL11-main\DISPLAYCPL\SHELL32\shell32.dll.mui %SYSTEMROOT%\System32\en-US\
+xcopy "Shell32.dll.mui" "%SYSTEMROOT%\System32\en-US"
 reg add HKEY_CLASSES_ROOT\CLSID\{C555438B-3C23-4769-A71F-B6D3D9B6053A}\Instance\InitPropertyBag /v ResourceDLL /t REG_EXPAND_SZ /d %SYSTEMROOT%\System32\Display.dll
 reg add HKEY_CLASSES_ROOT\CLSID\{C555438B-3C23-4769-A71F-B6D3D9B6053A}\Instance\InitPropertyBag /v ResourceID /t REG_DWORD /d 65
+@echo.
 @echo Done! You can now close this window.
+@echo.
 @explorer
-: to do
-: cleanup the code (ex. if youre in the windows folder, from cd %SYSTEMROOT%\Windows\System32 to cd System32
